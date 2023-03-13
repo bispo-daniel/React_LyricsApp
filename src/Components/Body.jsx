bd037c6425bfad7395a65ba15c3dd876caf871cc
bispo-daniel <dbispo.work@gmail.com>
2023-03-13 12:51:49 -0300

    updated
    .env

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,14 +4,16 @@ import '../Css/Body.css'
 class Body extends Component {
     render(){
         let iterator = 0
+        let it = 0
 
         const apiHandler = () => {
             //Pegando o valor dos inputs
             let song = document.getElementsByClassName('form-control')[0].value
             let artist = document.getElementsByClassName('form-control')[1].value
 
-            //Chave privada e URL da API
-            let key = 'CHANGE_WITH_YOURS'
+            //Chave privada e URL da API    
+            //GET YOUR KEY HERE: https://developer.musixmatch.com/admin/applications
+            let key = process.env.REACT_APP_SECRET_KEY
             let url = `http://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=${key}&q_track=${song}&q_artist=${artist}`
 
             //Pegando a div do html que recerá os valores em outra div
@@ -21,9 +23,13 @@ class Body extends Component {
             let queryDiv = document.createElement('div')
             //Seu ID único iterado
             queryDiv.setAttribute('id', `queryDiv${iterator}`)
+            iterator++
             //Inserindo na div do HTML
             insertInto.appendChild(queryDiv)
 
+            let p = document.createElement('p')
+            p.innerHTML = `Lyrics from Musixmatch S2...`
+            queryDiv.appendChild(p)
 
                 fetch(url)
                     .then(data => data.json())
@@ -47,9 +53,9 @@ class Body extends Component {
         //Função para limpar os resultados e tratando caso o usuário esqueça de usá-la
         const clearResults = () => {
             try{
-                const resultsDiv = document.getElementById(`queryDiv${iterator}`)
+                const resultsDiv = document.getElementById(`queryDiv${it}`)
                 resultsDiv.style.display = 'none'
-                iterator++
+                it++
             }catch(error) {
                 window.location.reload()
             }
@@ -66,6 +72,7 @@ class Body extends Component {
         return(
             <main onKeyUp={(e) => enterDown(e)}>
                 <h1>Only 30% ;)</h1>
+                <p>Download 'Allow CORS' browser extension...</p>
                 <div className="input-group">
                     <div className="input-group-append">
                         <input type="text" id="songInput" className="form-control" placeholder="Song" aria-label="Recipient's username" aria-describedby="basic-addon2" />
